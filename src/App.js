81252d751610e0e56d31e68baa04ceb2067f61ac
ganadara12 <rldnr314@gmail.com>
2023-09-20 00:23:32 +0900

    ver.4

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Signup from './components/Auth/Signup';
-import Signin from './components/Auth/Signin';
-import Todo from './components/Todo/TodoList';
+import React from 'react';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
-  const token = localStorage.getItem('token');
-
-  useEffect(() => {
-    if (token) {
-      if (window.location.pathname === '/signin' || window.location.pathname === '/signup') {
-        window.location.replace('/todo');
-      }
-    } else {
-      if (window.location.pathname === '/todo') {
-        window.location.replace('/signin');
-      }
-    }
-  }, [token]);
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/todo" element={<Todo />} />
-        <Route path="*" element={<Navigate to="/signin" />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <AppRoutes />
+    </div>
   );
 }
 
--- src/utils/api.js
+++
@@ -1,9 +0,0 @@
-// /utils/api.js
-
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://www.pre-onboarding-selection-task.shop/'
-});
-
-export default instance;
